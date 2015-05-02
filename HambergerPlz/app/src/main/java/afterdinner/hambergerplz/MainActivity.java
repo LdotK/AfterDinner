7203bf273655cd04830394c84240a0f869f10e7d
Yi JaeHo <ljhoo24@naver.com>
2015-05-02 19:38:05 +0900

    Activity Change Complete

--- HambergerPlz/app/src/main/java/afterdinner/hambergerplz/MainActivity.java
+++ HambergerPlz/app/src/main/java/afterdinner/hambergerplz/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +15,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnStart = (Button) findViewById(R.id.startBtn);
+        btnStart.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intentSubActivity =
+                        new Intent(MainActivity.this, hallActivity.class);
+                startActivity(intentSubActivity);
+                finish();
+            }
+        });
+
     }
 
 
@@ -20,12 +35,6 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    public void changeActivity(){
-        Intent intentHallActivity = new Intent(this,hallActivity.class);
-        startActivity(intentHallActivity);
-        finish();
     }
 
     @Override
